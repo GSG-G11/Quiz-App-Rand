50949dc829a162b62a6efda2ffcd0d44eb2a4857
Rand <randsohail98@gmail.com>
2022-01-20 16:12:31 +0200

    done first page starting the Quiz page

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hi");
+// let userName = [];
+
+// function getUserName() {
+//     let user = document.getElementById("user-name").value;
+//     userName.push(user);
+// }
+
+// console.log(userName);
+// // var subButton = document.getElementById('sub-button');
+// // subButton.addEventListener('click', getUserName, false);
+// var subButton = document.getElementById('sub-button');
+// subButton.addEventListener('click', getUserName, false);
+// subButton.onclick = function () {
+
+// }
+
+let userName = [];
+
+function passValue() {
+    let user = document.getElementById("user-name").value;
+    userName.push(user);
+
+}
+
